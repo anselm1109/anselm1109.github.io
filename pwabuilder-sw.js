9e76a683940a41818a70fd4051df760b7491c918
anselm1109 <anselm1109a@gmail.com>
2020-04-15 13:38:35 -0600

    Fix for Iphone

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,9 +1,8 @@
-// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+// This is the "serving cached media" service worker
 
-const CACHE = "pwabuilder-offline-page";
+const CACHE = "pwabuilder-offline";
+
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "offline.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -11,47 +10,21 @@ self.addEventListener("message", (event) => {
   }
 });
 
-self.addEventListener('install', async (event) => {
-  event.waitUntil(
-    caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
-  );
-});
-
-if (workbox.navigationPreload.isSupported()) {
-  workbox.navigationPreload.enable();
-}
+workbox.loadModule('workbox-cacheable-response');
+workbox.loadModule('workbox-range-requests');
 
 workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
+  /.*\.mp4/,
+  new workbox.strategies.CacheFirst({
+    cacheName: CACHE,
+    plugins: [
+      new workbox.cacheableResponse.CacheableResponsePlugin({statuses: [200]}),
+      new workbox.rangeRequests.RangeRequestsPlugin(),
+    ],
+  }),
 );
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
-
-        const networkResp = await fetch(event.request);
-        return networkResp;
-      } catch (error) {
-
-        const cache = await caches.open(CACHE);
-        const cachedResp = await cache.match(offlineFallbackPage);
-        return cachedResp;
-      }
-    })());
-  }
-});
-
-caches.keys().then(function(names) {
-  for (let name of names)
-      caches.delete(name);
-});
+// caches.keys().then(function(names) {
+//   for (let name of names)
+//       caches.delete(name);
+// });
